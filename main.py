b5cb853bf1b21d97e87dcf5dbbdc776f26b323b2
ogirodabola <fsr.tiago@gmail.com>
2026-01-27 07:38:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 from core.database import criar_tabelas
 from core.database import listar_noticias, listar_categorias
+from crawler.crawler_noticias import rodar_crawler
+
+@app.get("/_run-crawler")
+def run_crawler():
+    rodar_crawler()
+    return {"status": "crawler executado"}
 
 BASE_DIR = Path(__file__).resolve().parent
 
