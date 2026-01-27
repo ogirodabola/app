d13d90587616826638670ba02fbed41aeb8ac4ad
ogirodabola <fsr.tiago@gmail.com>
2026-01-27 00:31:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, PlainTextResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+
+from core.database import listar_noticias, listar_categorias
 
 BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
 
-# STATIC
 app.mount(
     "/static",
     StaticFiles(directory=BASE_DIR / "static"),
     name="static"
 )
 
-# TEMPLATES
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-# TESTE ABSOLUTO (SEM JINJA)
-@app.get("/ping", response_class=PlainTextResponse)
-def ping():
-    return "pong"
 
-# HOME
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+@app.get("/")
+def home(request: Request, categoria: str = None):
+    noticias = listar_noticias(limit=30, categoria=categoria)
+    categorias = listar_categorias()
+
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {
+            "request": request,
+            "noticias": noticias,
+            "categorias": categorias,
+            "categoria_ativa": categoria
+        }
     )
