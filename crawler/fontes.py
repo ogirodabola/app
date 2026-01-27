8b51dc811d4fdaa3fc5ef465007d44f350b363f7
ogirodabola <fsr.tiago@gmail.com>
2026-01-26 23:09:52 -0300

    Create fontes.py

---
+++ crawler/fontes.py
@@ -0,0 +1,17 @@
+FONTES = [
+    {
+        "nome": "GE",
+        "url": "https://ge.globo.com/futebol/",
+        "tipo": "html"
+    },
+    {
+        "nome": "UOL Esporte",
+        "url": "https://www.uol.com.br/esporte/futebol/",
+        "tipo": "html"
+    },
+    {
+        "nome": "ESPN Brasil",
+        "url": "https://www.espn.com.br/futebol/",
+        "tipo": "html"
+    }
+]
