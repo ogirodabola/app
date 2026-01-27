70b3d19abe2e16d99aff6489e6caacc17527c13a
ogirodabola <fsr.tiago@gmail.com>
2026-01-27 10:08:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,6 @@ app = FastAPI()
 # garante que o banco e as tabelas existam
 criar_tabelas()
 
-from core.database import limpar_lixo
-
-criar_tabelas()
-limpar_lixo()
-
-
 app.mount(
     "/static",
     StaticFiles(directory=BASE_DIR / "static"),
