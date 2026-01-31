7bebc2b7b70adab93de052f75361916dd2b2a272
ogirodabola <fsr.tiago@gmail.com>
2026-01-31 15:41:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,24 +36,20 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 # ======================================================
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+    ultimas = listar_ultima_hora(5)
+
+    print("DEBUG ULTIMAS:", ultimas)  # 👈 IMPORTANTE
+
     return templates.TemplateResponse(
         "index.html",
         {
             "request": request,
-
-            "ultimas_noticias": listar_ultima_hora(5),
+            "ultimas_noticias": ultimas,
             "ultima_hora": listar_ultima_hora(6),
-            "brasileirao": listar_por_categoria("Brasileirão", 8),
-            "mercado": listar_por_categoria("Mercado da Bola", 6),
-            "analises": listar_por_categoria("Análises", 5),
-            "bastidores": listar_por_categoria("Bastidores", 5),
-
             "categorias": listar_categorias(),
             "categoria_ativa": None
         }
     )
-
-
 
 # ======================================================
 # LISTAGEM POR CATEGORIA
