a7fc043229995d8a3b6c6aed5aa6a604162bed21
ogirodabola <fsr.tiago@gmail.com>
2026-02-18 09:41:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ from core.database import (
     listar_editorial_publicado,
     listar_noticias_publicadas,
     buscar_ou_criar_jogador,
-    salvar_relacao_noticia_jogador,
     limpar_vinculos_jogadores_noticia,
     buscar_jogador_por_slug,
     criar_jogador_basico,
