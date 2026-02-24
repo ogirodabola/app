3de3446b289608a64aad195a259c51b027e0d1cd
ogirodabola <fsr.tiago@gmail.com>
2026-02-24 14:06:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1127,3 +1127,25 @@ def politica_correcao(request: Request):
             "categoria_ativa": None
         }
     )
+
+from core.database import buscar_autor_por_slug, listar_noticias_por_autor
+
+@app.get("/autor/{slug}", response_class=HTMLResponse)
+def pagina_autor(slug: str, request: Request):
+
+    autor = buscar_autor_por_slug(slug)
+
+    if not autor:
+        raise HTTPException(status_code=404)
+
+    noticias = listar_noticias_por_autor(autor["id"], limit=12)
+
+    return templates.TemplateResponse(
+        "autor.html",
+        {
+            "request": request,
+            "autor": autor,
+            "noticias": noticias,
+            "total_materias": len(noticias),
+        }
+    )
