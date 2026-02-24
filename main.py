25e0bf02d4bb375cc4cea1b8e8f392d972ce2528
ogirodabola <fsr.tiago@gmail.com>
2026-02-24 14:17:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1080,42 +1080,10 @@ def pagina_jogador(slug: str, request: Request):
 
 from core.database import buscar_jogadores_por_nome
 
-@app.get("/autor/tiago-sampaio", response_class=HTMLResponse)
-def autor_tiago(request: Request):
-    return templates.TemplateResponse(
-        "autor_tiago.html",
-        {
-            "request": request,
-            "categorias": listar_categorias(),
-            "categoria_ativa": None
-        }
-    )
+
 @app.get("/admin/buscar-jogador")
 def buscar_jogador_api(query: str):
     return buscar_jogadores_por_nome(query)
-
-
-from core.database import buscar_autor_por_slug, listar_noticias_por_autor
-
-@app.get("/autor/{slug}", response_class=HTMLResponse)
-def pagina_autor(slug: str, request: Request):
-
-    autor = buscar_autor_por_slug(slug)
-    if not autor:
-        raise HTTPException(status_code=404)
-
-    noticias = listar_noticias_por_autor(slug)
-
-    return templates.TemplateResponse(
-        "autor.html",
-        {
-            "request": request,
-            "autor": autor,
-            "noticias": noticias,
-            "categorias": listar_categorias(),
-            "categoria_ativa": None
-        }
-    )
 
 @app.get("/politica-de-correcao", response_class=HTMLResponse)
 def politica_correcao(request: Request):
